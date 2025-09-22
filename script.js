11afd7a17d33e4126f8bd4d75fca7d694bee48a0
hushus23 <97086514+hushus23@users.noreply.github.com>
2025-09-22 15:30:25 +0700

    responsive added

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    const shareBtn = document.getElementById('share-btn');
+    const shareBar = document.getElementById('share-bar');
+
+    shareBtn.addEventListener('click', () => {
+      shareBar.classList.toggle('hidden');
+    });
